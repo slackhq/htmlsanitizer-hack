c6c3ba78a6b6df971a6c794a48cfeded414e13ab
Jack Wilson <jack.wilson.v@gmail.com>
2021-01-20 14:04:06 -0800

    Try something else for output

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Created by Jack Wilson on 01/20/2021
 
-echo "testing"
-vendor/bin/hacktest tests/
-echo "testing again"
+output="$(vendor/bin/hacktest tests/)";
+if $?
+then
+  echo "success";
+  exit 0;
+else
+  echo "failure";
+  echo $output;
+  exit 1;
+fi
